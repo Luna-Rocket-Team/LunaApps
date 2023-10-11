5ae4a2139dda8fc7e022d56d8a588775ac25e0f5
Matheus M. Parrela <92748188+matheusparrela@users.noreply.github.com>
2023-10-11 00:26:39 -0300

    Update CurvaEmpuxo.kt
    Adicionado o método que calcula o tempo total de queima.

--- src/main/kotlin/curvaEmpuxo/CurvaEmpuxo.kt
+++ src/main/kotlin/curvaEmpuxo/CurvaEmpuxo.kt
@@ -82,4 +82,8 @@ class CurvaEmpuxo(var file: String, var min_empuxo: Double, var prop_massa: Doub
     fun fluxoMassaMedio(): Double{
         return this.prop_massa/(this.axis_x[this.axis_x.size-1] - this.axis_x[0])
     }
+    
+    fun tempoQueima(): Double{
+        return this.axis_x[this.axis_x.size-1] - this.axis_x[0]
+    }
 }
